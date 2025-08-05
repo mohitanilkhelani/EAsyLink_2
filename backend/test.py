42633d73faac293afe81a2cd3ce1d14ddc69cd47
mohitanilkhelani <mohitanil.khelani@bosch.com>
2025-08-05 18:17:48 +0800

    Delete backend directory

--- backend/test.py
+++
@@ -1,7 +0,0 @@
-import pyodbc
-
-conn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=SGPVSQL51.apac.bosch.com;DATABASE=DB_BDO14_SQL;UID=BDOCMP;PWD=BDOCMPSingapore2024+'
-)
-print("Connected!")
-conn.close()
